4d2a0b892897c4b313662ec944f7c344656780e8
Henry Blevins <heblevi@gmail.com>
2019-05-13 12:31:20 -0400

    Implement and test repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,34 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('repair', () => {
+  it('reset durability', () => {
+    const item = {
+      durability: 75,
+      name: "test",
+      enhancement: 20
+    };
+    const newItem = enhancer.repair(item);
+    expect(newItem.durability).toBe(100);
+  });
+  it('only change durability', () => {
+    const item = {
+      durability: 75,
+      name: "test",
+      enhancement: 20
+    };
+    const newItem = enhancer.repair(item);
+    expect(newItem.name).toBe("test");
+    expect(newItem.enhancement).toBe(20);
+  });
+  it('new object', () => {
+    const item = {
+      durability: 75,
+      name: "test",
+      enhancement: 20
+    };
+    enhancer.repair(item);
+    expect(item.durability).toBe(75);
+    expect(item.name).toBe("test");
+    expect(item.enhancement).toBe(20);
+  })
+});
